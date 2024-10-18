cda3f7c3368bc9ac582be495e2f7f5806e7ab671
mayanksri02 <srimayank2002@gmail.com>
2024-10-18 14:15:54 +0530

    js work

---
+++ JS/index.js
@@ -0,0 +1,64 @@
+// console.log("hello");
+// var a=12;
+// console.log("a="+a);
+// if (a>10){
+//     let a=30
+//     console.log("hiii",a);
+// }
+// console.log("hoiii",a);
+
+// let sim = Symbol("qjiwqjdiow000");
+// console.log(typeof sim);
+// let today=Date();
+// console.log(today);
+// console.log(typeof today);
+
+// let a="12"
+// let b=12
+// if (a==b){
+//     console.log("value equal");
+// }
+// if (a===b){
+//     console.log("data type equal");
+// }
+// else{
+//     console.log("not equal");
+// }
+
+// function hi(str){
+//     console.log("hello "+str);
+// }
+// hi("jlllllll");
+// let d=function hi(str){
+//     console.log("hello "+str);
+// }
+// d();
+// function sum(a,b){
+//     return a+b;
+// }
+// let total=sum(24,27);
+// console.log(total)
+
+
+// function selectlang(lang){
+//     function ccomp(){
+//         return "c compiler selected";
+//     }
+//     function javacomp(){
+//         return "java compiler selected";
+//     }
+//     if (lang=="C"){
+//         return ccomp();
+//     }
+//     else{
+//         return javacomp();
+//     }
+// }
+// console.log(selectlang("java"))
+
+// console.log(selectlang())
+
+console.log("Hello JS")
+console.dir(document)
+let parent=document.getElementsByClassName("parent");
+console.dir(parent)
