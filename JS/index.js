3c41394a3b668f2043fa4d1010a1e5a2e06451c7
mayanksri02 <srimayank2002@gmail.com>
2024-10-18 15:41:52 +0530

    dom and promises

--- JS/index.js
+++ JS/index.js
@@ -58,7 +58,77 @@
 
 // console.log(selectlang())
 
-console.log("Hello JS")
-console.dir(document)
+// console.log("Hello JS")
+// console.dir(document)
 let parent=document.getElementsByClassName("parent");
-console.dir(parent)
+// console.log(parent)
+// parent[0].innerText="ABES";
+// parent[0].innerHTML="<h2>ABES</h2>";
+// const h1=document.createElement("h1")
+
+// h1.innerText="ELEMENT US CREATED BY DOM";
+// parent[0].appendChild(h1);
+// h1.style.background="yellow";
+// h1.style.color="blue";
+
+// const img=document.createElement('img');
+// img.src='../cat.jpg'
+// img.setAttribute("height","200px")
+// console.log(img);
+// parent[0].appendChild(img);
+// const button=document.getElementById("btn");
+// const display=document.getElementById("disp");
+// getData=()=> {
+//     console.log("calling getdata fn");
+//     display.innerHTML="<h3>Welcome to CSE</h3>"
+// }
+
+
+// button.addEventListener("click",() => {
+//     getData();
+// })
+
+// const promise1=new Promise(
+//     (resolve,reject)=> {
+//         let a=Math.random()*200;
+//         console.log(a);
+        
+//         if(a>=100 && a<=200){
+//             resolve(`number is ${a} value is between 100 and 200`);
+//         }else{
+//             reject(`number is ${a} value is not between 100 and 200`);
+//         }
+//     }
+// );
+
+// promise1.then((msg)=>{console.log(msg);})
+// .catch(error=> {console.log("error")})
+// .finally(msg=>{console.log("All resources have been closed");});
+
+
+// const promise2=new Promise(
+//     (resolve,reject)=> {
+//         resolve({name:"mayank",branch:"CSE"})
+//     }
+// );
+// promise2.then((data)=>{console.log(data.name)})
+// .catch(error=> {console.log(error)})
+
+
+const response=fetch("https://dummyjson.com/products")
+response.then(data=> {
+    console.log(data);
+    data.json().then(res=>{
+        // console.log(res.products[0].title+ "  "+ res.products[0].id)
+        // const myData=res.products[0].title+ "  "+ res.products[0].id;
+        // console.log(res);
+        res.products.map((title)=> (
+            console.log(title)
+            
+            // parent[0].innerHTML=`<h2> ${title} </h2>`
+
+        ))
+        
+    })
+
+})
---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+const promise1=new Promise(
+//     (resolve,reject)=> {
+//         let a=Math.random()*200;
+//         console.log(a);
+        
+//         if(a>=100 && a<=200){
+//             resolve(`number is ${a} value is between 100 and 200`);
+//         }else{
+//             reject(`number is ${a} value is not between 100 and 200`);
+//         }
+//     }
+// );
+
+// promise1.then((msg)=>{console.log(msg);})
+// .catch(error=> {console.log("error")})
+// .finally(msg=>{console.log("All resources have been closed");});
